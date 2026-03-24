102811c95424447859d6373dbf9e10fc5d729e78
sijirama <gbemilesanmi@gmail.com>
2026-03-24 13:59:46 +0100

    val los every to 0

--- train_gpt.py
+++ train_gpt.py
@@ -47,7 +47,7 @@ class Hyperparameters:
 
     # Validation cadence and batch size. Validation always uses the full fineweb_val split.
     val_batch_size = int(os.environ.get("VAL_BATCH_SIZE", 524_288))
-    val_loss_every = int(os.environ.get("VAL_LOSS_EVERY", 3000))
+    val_loss_every = int(os.environ.get("VAL_LOSS_EVERY", 0))
     train_log_every = int(os.environ.get("TRAIN_LOG_EVERY", 200))
 
     # Training length.
